362331bf60b1686872839c0b547fe84ed0fc3a4b
HCCCCC <837647312@qq.com>
2021-11-15 19:48:08 +0800

    Framework Base版本

---
+++ FrameworkDesign/Example/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace FrameworkDesign.Example
+{
+    public class Enemy : MonoBehaviour
+    {
+        public GameObject GamePassPanel;
+
+        private static int mKilledEnemyCount = 0;
+
+        private void OnMouseDown()
+        {
+            Destroy(gameObject);
+            mKilledEnemyCount++;
+
+            if (mKilledEnemyCount == 10)
+                GamePassPanel.SetActive(true);
+        }
+    }   
+}
+
+
