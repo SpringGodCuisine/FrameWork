37f753fca9ca044e25df13798c1c9b614c6d2157
HCCCCC <837647312@qq.com>
2021-11-24 12:48:57 +0800

    添加IOC容器

---
+++ FrameworkDesign/Example/IOC/IOCExample.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace FrameworkDesign.Example
+{
+    public class IOCExample : MonoBehaviour
+    {
+        private void Start()
+        {
+            var container = new IOCContainer();
+
+            container.Register(new BluetoothManager());
+
+            var bluetoothManager = container.Get<BluetoothManager>();
+
+            bluetoothManager.Connect();
+        }
+
+        public class BluetoothManager
+        {
+            public void Connect()
+            {
+                Debug.Log("蓝牙连接成功");
+            }
+        }
+    }
+}
+
+
